bde97adb46611f95921aea480954e2a663e6ab1c
plsantoro <plsantoro@hotmail.com>
2017-08-14 22:47:57 -0300

    66 porcento

--- src/main.c
+++ src/main.c
@@ -8,15 +8,35 @@
 #include <stdio.h>
 
 int main() {
+	char letra1=0, letra2=0;
+	int n = 0, c_especial = 0; 
 
-  char c;
+	
+	while (letra1 != '\n') {	/*Loop para leitura da frase*/
+    
+		scanf("%c", &letra1);
+    
+		if(letra1>=32 && letra1<=47){ 
+   			c_especial=1;	/*flag ativa para caractere especial*/
+     		}
+		
+		///mais uma palavra se o letra1 eh letra ou numero e letra2 pontuacao
+		else if(((c_especial==1) && (letra1 <= 122 && letra1 >= 65)) && (!((letra1>=48 && letra1<=57) && (letra2 == '.' ||  letra2 == ',')))){
+        		n++;
+      			c_especial = 0;
+      		}
+		
+		else{
+       			c_especial = 0; /// caso de float
+     		}
 
-  c = 1;
+    		letra2 = letra1; //salva caracter anterior para comparacao na proxima iteracao
 
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
+	}
+	
+	n++;
+  
+	printf("%d\n", n); //imprime o valor n de palavras da frase
 
-  printf("5\n");
-  return 0;
+	return 0;
 }
